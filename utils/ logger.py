be450af8e287558c5242b9ebe7a2124d4b6718ae
daniele.pisciottano <118293813+maddaniello@users.noreply.github.com>
2025-12-03 08:49:16 +0100

    Update  logger.py

--- utils/ logger.py
+++ utils/ logger.py
@@ -2,30 +2,20 @@
 Logger centralizzato con Loguru
 """
 import sys
-from pathlib import Path
-from loguru import logger
-from config import LOGS_DIR, LOG_LEVEL, LOG_FORMAT, LOG_ROTATION, LOG_RETENTION
+from loguru import logger as loguru_logger
 
 # Rimuovi handler default
-logger.remove()
+loguru_logger.remove()
 
 # Console handler con colori
-logger.add(
+loguru_logger.add(
     sys.stdout,
-    format=LOG_FORMAT,
-    level=LOG_LEVEL,
+    format="<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan> - <level>{message}</level>",
+    level="INFO",
     colorize=True,
 )
 
-# File handler con rotazione
-logger.add(
-    LOGS_DIR / "app_{time:YYYY-MM-DD}.log",
-    format=LOG_FORMAT,
-    level=LOG_LEVEL,
-    rotation=LOG_ROTATION,
-    retention=LOG_RETENTION,
-    compression="zip",
-)
-
 # Export
+logger = loguru_logger
+
 __all__ = ["logger"]
